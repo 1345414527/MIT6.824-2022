100353f961164ba2a2545dbe861e0db4553ee5a3
jiangchaolun <1345414527@qq.com>
2022-08-26 17:54:28 +0800

    其它文件，不需要管

---
+++ main/test-mr-early.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+
+#
+# map-reduce tests
+#
+
+# comment this out to run the tests without the Go race detector.
+RACE=-race
+
+if [[ "$OSTYPE" = "darwin"* ]]
+then
+  if go version | grep 'go1.17.[012345]'
+  then
+    # -race with plug-ins on x86 MacOS 12 with
+    # go1.17 before 1.17.6 sometimes crash.
+    RACE=
+    echo '*** Turning off -race since it may not work on a Mac'
+    echo '    with ' `go version`
+  fi
+fi
+
+TIMEOUT=timeout
+if timeout 2s sleep 1 > /dev/null 2>&1
+then
+  :
+else
+  if gtimeout 2s sleep 1 > /dev/null 2>&1
+  then
+    TIMEOUT=gtimeout
+  else
+    # no timeout command
+    TIMEOUT=
+    echo '*** Cannot find timeout command; proceeding without timeouts.'
+  fi
+fi
+if [ "$TIMEOUT" != "" ]
+then
+  TIMEOUT+=" -k 2s 180s "
+fi
+
+# run the test in a fresh sub-directory.
+rm -rf mr-tmp
+mkdir mr-tmp || exit 1
+cd mr-tmp || exit 1
+rm -f mr-*
+
+# make sure software is freshly built.
+(cd ../../mrapps && go clean)
+(cd .. && go clean)
+(cd ../../mrapps && go build $RACE -buildmode=plugin wc.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin indexer.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin mtiming.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin rtiming.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin jobcount.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin early_exit.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin crash.go) || exit 1
+(cd ../../mrapps && go build $RACE -buildmode=plugin nocrash.go) || exit 1
+(cd .. && go build $RACE mrcoordinator.go) || exit 1
+(cd .. && go build $RACE mrworker.go) || exit 1
+(cd .. && go build $RACE mrsequential.go) || exit 1
+
+failed_any=0
+
+#########################################################
+
+#########################################################
+# test whether any worker or coordinator exits before the
+# task has completed (i.e., all output files have been finalized)
+rm -f mr-*
+
+echo '***' Starting early exit test.
+
+DF=anydone$$
+rm -f $DF
+
+($TIMEOUT ../mrcoordinator ../pg*txt ; touch $DF) &
+
+# give the coordinator time to create the sockets.
+sleep 1
+
+# start multiple workers.
+($TIMEOUT ../mrworker ../../mrapps/early_exit.so ; touch $DF) &
+sleep 1
+($TIMEOUT ../mrworker ../../mrapps/early_exit.so ; touch $DF) &
+sleep 1
+($TIMEOUT ../mrworker ../../mrapps/early_exit.so ; touch $DF) &
+
+# wait for any of the coord or workers to exit.
+# `jobs` ensures that any completed old processes from other tests
+# are not waited upon.
+jobs &> /dev/null
+if [[ "$OSTYPE" = "darwin"* ]]
+then
+  # bash on the Mac doesn't have wait -n
+  while [ ! -e $DF ]
+  do
+    sleep 0.2
+  done
+else
+  # the -n causes wait to wait for just one child process,
+  # rather than waiting for all to finish.
+  wait -n
+fi
+
+rm -f $DF
+
+# a process has exited. this means that the output should be finalized
+# otherwise, either a worker or the coordinator exited early
+sort mr-out* | grep . > mr-wc-all-initial
+echo
+# wait for remaining workers and coordinator to exit.
+wait
+
+# compare initial and final outputs
+sort mr-out* | grep . > mr-wc-all-final
+if cmp mr-wc-all-final mr-wc-all-initial
+then
+  echo '---' early exit test: PASS
+else
+  echo '---' output changed after first worker exited
+  echo '---' early exit test: FAIL
+  failed_any=1
+fi
+#rm -f mr-*
